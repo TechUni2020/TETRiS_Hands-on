510fdfca6fa39ae07c808e9122077c5e237aeea7
ide Shouhi <shouhi0922@icloud.com>
2021-07-18 13:51:01 +0900

    :sparkles:9/2

---
+++ 01-9-2/index.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("btn1");
+btn.onclick = function click() {
+  //九九表の作成
+  for (let i = 1; i < 10; i++) {
+    let arr = "";
+    for (let j = 1; j < 10; j++) {
+      arr += i * j;
+      arr += " ";
+    }
+    console.log(arr);
+  }
+};
